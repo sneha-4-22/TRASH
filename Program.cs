f0fa3865ec96d7d8d49c78208b991399bd5d6cee
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2025-07-21 21:46:19 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
+using crud_App.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options =>
+// DB connection from appsettings
+builder.Services.AddDbContext<GoalDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Enable CORS for frontend
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
-app.UseRouting();
+
+app.UseCors("AllowReactApp");
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
