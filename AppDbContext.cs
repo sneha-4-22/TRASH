885239ec578d15e718088185e36d577affb46e54
SNEHA KUMARI <112711068+sneha-4-22@users.noreply.github.com>
2025-08-07 11:29:03 +0530

    Delete AppDbContext.cs

--- AppDbContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<Goal> Goals { get; set; }
-}
